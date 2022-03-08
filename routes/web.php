cfa95bc6e4cd4517f5e87b1a086e5735f3cb0ffa
Martin Bojmaliev <mbojmaliev@gmail.com>
2022-03-08 11:33:36 +0100

    added posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    if (Post::get()->count() === 0) {
+        $post1 = Post::create(['name' => 'test1']);
+        $post2 = Post::create(['name' => 'test2']);
+        $post3 = Post::create(['name' => 'test3']);
+        $post4 = Post::create(['name' => 'test4']);
+
+        views($post1)->record();
+        views($post1)->record();
+        views($post1)->record();
+
+        views($post2)->record();
+        views($post2)->record();
+        views($post2)->record();
+        views($post2)->record();
+
+        views($post4)->record();
+        views($post4)->record();
+        views($post4)->record();
+        views($post4)->record();
+        views($post4)->record();
+        views($post4)->record();
+
+
+        views($post3)->record();
+        views($post3)->record();
+        views($post3)->record();
+        views($post3)->record();
+    }
+
+    return Post::orderByViews()->cursorPaginate(2);
+
 });
